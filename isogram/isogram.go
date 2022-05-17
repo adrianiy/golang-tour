95893d61b128d05add96c0425cc828c9aaf7ffad
Adrián Insua Yañez <adrianiy@ext.inditex.com>
2022-05-17 02:50:22 +0200

    fix: faster implementation using string methods

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,22 +1,20 @@
 // Package isogram expose a function to determine if a word is an isogram or not.
 package isogram
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 // IsIsogram creates a map with letter repetition and return true if the maximum value is 1
 func IsIsogram(word string) bool {
-	accum := map[rune]int8{}
+	word = strings.ToLower(word)
 
-	for _, letter := range strings.ToLower(word) {
-		if letter != '-' && letter != ' ' {
-			accum[letter]++
-
-			if accum[letter] > 1 {
-				return false
-			}
+	for i, letter := range word {
+		if unicode.IsLetter(letter) && strings.ContainsRune(word[i+1:], letter) {
+			return false
 		}
 
 	}
-
 	return true
 }
